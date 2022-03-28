6b55a2e03700059b53a38b0a380ac164b284c0d7
rafael cavalcante <ra.facavalcante@hotmail.com>
2022-03-28 08:53:36 -0300

    fix: fix para inserir as dependecias nos testes

--- src/usuario/usuario.controller.spec.ts
+++ src/usuario/usuario.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { UsuarioController } from './usuario.controller'
+import { UsuarioService } from './usuario.service'
 
 describe('UsuarioController', () => {
   let controller: UsuarioController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsuarioController]
+      controllers: [UsuarioController],
+      providers: [UsuarioService]
     }).compile()
 
     controller = module.get<UsuarioController>(UsuarioController)
