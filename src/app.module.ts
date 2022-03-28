969feced62da95fa54b17fd5c9dcd91d8c837d05
rafael cavalcante <ra.facavalcante@hotmail.com>
2022-03-27 23:52:29 -0300

    feat(usuarios): criando controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { UsuarioController } from './usuario/usuario.controller'
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService]
+  controllers: [UsuarioController],
+  providers: []
 })
 export class AppModule {}
