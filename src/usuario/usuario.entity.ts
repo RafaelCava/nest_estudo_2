19508eb9c71f55d7bb14ce5629f66ef65d1fd143
rafael cavalcante <ra.facavalcante@hotmail.com>
2022-03-29 01:26:01 +0000

    feat(pipes): utilizando pipes

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,8 +1,28 @@
+import { IsEmail, IsNotEmpty, IsNumber, IsString } from 'class-validator'
+
 export class Usuario {
+  @IsNumber()
   id: number
+
+  @IsNotEmpty({
+    message: 'O nome de usuário não pode ser vazio'
+  })
+  @IsString()
   nomeDeUsuario: string
+
+  @IsEmail()
   email: string
+
+  @IsNotEmpty({
+    message: 'O campo senha é obrigatório'
+  })
+  @IsString()
   senha: string
+
+  @IsNotEmpty({
+    message: 'O nome completo é obrigatório'
+  })
+  @IsString()
   nomeCompleto: string
   dataDeEntrada: Date
 }
